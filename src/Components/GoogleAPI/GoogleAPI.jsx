9cdb1cc1a5baac51a22a3aae7389f40d465ed7ae
Axialis <63449452+Axialis@users.noreply.github.com>
2022-04-09 17:19:47 +0300

    fix: google clientID

--- src/Components/GoogleAPI/GoogleAPI.jsx
+++ src/Components/GoogleAPI/GoogleAPI.jsx
@@ -7,11 +7,6 @@ const GoogleAPI = () => {
     let [name, setName] = useState(null);
     let [logState, setLogState] = useState(false);
 
-    const customStyle =
-    {
-        width: '500px',
-
-    }
     const onSuccess = (res) => {
         setName(res.profileObj.name);
         setLogState(true)
@@ -27,21 +22,18 @@ const GoogleAPI = () => {
     return (
         <div className={s.google}>
             {!logState && <GoogleLogin
-                clientId={'315590259705-u4otem6a8tge44qa858e7ol4nem4rt64'}
+                clientId={'315590259705-u4otem6a8tge44qa858e7ol4nem4rt64.apps.googleusercontent.com'}
                 buttonText="Login"
                 onSuccess={onSuccess}
                 onFailure={onFailure}
                 cookiePolicy={'single_host_origin'}
-                style={customStyle}
             />}
             {!!logState && <div className={s.text}>{name}</div>}
             {!!logState && <GoogleLogout
-                clientId={'315590259705-u4otem6a8tge44qa858e7ol4nem4rt64'}
+                clientId={'315590259705-u4otem6a8tge44qa858e7ol4nem4rt64.apps.googleusercontent.com'}
                 buttonText={"LogOut"}
                 onLogoutSuccess={logOut}
-                style={customStyle}
             />}
-
         </div>
     )
 
